ac1ca3ccb80fffec10785b28ede467b2ad0eef61
alejandro <alejandrober96@gmail.com>
2023-08-04 16:28:54 -0500

    Add button for adding tasks

--- src/components/StatusColumn.tsx
+++ src/components/StatusColumn.tsx
@@ -1,10 +1,14 @@
+'use client'
 import styles from './statusColumns.module.css'
 
-
 const StatusColumn = ({ title }: {title:string}) => {
+  const handleAddTask = () => {
+    console.log('add task');
+  }
   return (
     <div className={styles.column}>
       <h2>{title}</h2>
+      <button className={styles.addTask} onClick={handleAddTask}>Add Task +</button>
       <div className={styles.tasksColumn}></div>
     </div>
   )
