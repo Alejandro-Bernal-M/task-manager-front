e6c86f6d6fdbca8931908f20df0f1c98e859a78b
Alejandro Bernal Marin <alejandrober96@gmail.com>
2023-08-14 14:07:00 -0500

    Create groups component and add it to the nav bar

---
+++ src/app/groups/page.tsx
@@ -0,0 +1,11 @@
+import styles from './groups.module.css';
+
+const Groups = () => {
+  return (
+    <div className={styles.container}>
+      <h1>Groups</h1>
+    </div>
+  );
+};
+
+export default Groups;
