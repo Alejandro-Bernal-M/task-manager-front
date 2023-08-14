3cd2ec111f5ce8490b786a99a604c943966c86a7
Alejandro Bernal Marin <alejandrober96@gmail.com>
2023-08-14 14:31:22 -0500

    Corrections on groups layout

--- src/app/groups/page.tsx
+++ src/app/groups/page.tsx
@@ -1,4 +1,11 @@
+'use client'
+
 import styles from './groups.module.css';
+import { toast } from 'react-hot-toast';
+
+const handleAddGroup = () => {
+  toast.success('Group added successfully');
+}
 
 const Groups = () => {
   return (
@@ -7,6 +14,7 @@ const Groups = () => {
         <div className={styles.groupHolder}>
           <h2>Groups Created by you</h2>
           <hr />
+          <button className={styles.addGroup} onClick={handleAddGroup}>Add Group +</button>
         </div>
         <div className={styles.groupHolder}>
           <h2>Sub-groups where you are in</h2>
