e62f1f4f5ff756a3c9a86df0ae817338a76840d2
Alejandro Bernal Marin <alejandrober96@gmail.com>
2023-08-14 14:18:25 -0500

    Add basic layout on the groups page

--- src/app/groups/page.tsx
+++ src/app/groups/page.tsx
@@ -3,7 +3,16 @@ import styles from './groups.module.css';
 const Groups = () => {
   return (
     <div className={styles.container}>
-      <h1>Groups</h1>
+      <div className={styles.mainHolder}>
+        <div className={styles.groupHolder}>
+          <h2>Groups Created by you</h2>
+          <hr />
+        </div>
+        <div className={styles.groupHolder}>
+          <h2>Sub-groups where you are in</h2>
+          <hr />
+        </div>
+      </div>
     </div>
   );
 };
