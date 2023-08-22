da1176f8d204935fadb2519d6cd43dc71b75e764
alejandro <alejandrober96@gmail.com>
2023-08-22 17:01:01 -0500

    Add fetching invitations data

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -9,6 +9,8 @@ import { useEffect, useState } from 'react';
 const Profile = () =>{
     const { setLoggedIn } = useStateContext();
     const [user, setUser] = useState({} as any);
+    const [invitations, setInvitations] = useState([] as any);
+
     let tokenString = localStorage.getItem('token') || '';
     if (!tokenString) {
       toast.error('Your session has expired, please login again');
@@ -50,18 +52,53 @@ const Profile = () =>{
         }
       }
       fetchUser();
-    }, [setLoggedIn, token, urlUser]);
+      const fetchInvitations = async () => {
+        try {
+          const response = await fetch(api.invitations(userId), {
+            method: 'GET',
+            headers: {
+              'Content-Type': 'application/json',
+              'Authorization': token
+            }
+          })
+          const data = await response.json();
+          console.log(data)
+          if(data.status === 'SUCCESS'){
+            setInvitations(data.data);
+          }
+        } catch (error) {
+          console.log(error)
+        }
+      }
+      fetchInvitations();
+    }, [setLoggedIn, token, urlUser, userId]);
+
 
 
     return(
-        <div className={styles.container}>
-            <h1>Your Profile</h1>
-            <div className={styles.userContainer}>
-                <p>Name: {user.name}</p>
-                <p>Email: {user.email}</p>
-                <button>Edit</button>
-            </div>
+      <div className={styles.container}>
+        <h1>Your Profile</h1>
+        <div className={styles.mainHolder}>
+          <div className={styles.userContainer}>
+              <p>Name: {user.name}</p>
+              <p>Email: {user.email}</p>
+              <button>Edit</button>
+          </div>
+          <div className={styles.invitationsContainer}>
+            <h2>Your Invitations</h2>
+            {invitations.length > 0 ? invitations.map((invitation: any) => (
+              <div key={invitation.id}>
+                <p>invited to:{invitation.subgroup}</p>
+                <p>invited by:</p>
+                <p>name: {invitation.invited_by.name}</p>
+                <p>email: {invitation.invited_by.email}</p>
+                <button>Accept</button>
+                <button>Reject</button>
+              </div>
+                )) : <p>No Invitations</p>}
+          </div>
         </div>
+      </div>
     )
 }
 
